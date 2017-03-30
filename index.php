44ec2b24b8d5340a54070af072a0db9fa19612c3
Lucas R <spallacety@hotmail.com>
2017-03-30 20:09:04 -0300

    Alterações layout páginas

--- index.php
+++ index.php
@@ -8,22 +8,23 @@
 <hr/>
 <div id="form">
   <form action="home.php" method="post">
-    <div class="row row-centered">
-    <div class="col-md-4 col-centered">
-    <label for = "usuario">Usuário:</label>
-    <input class="form-control" type = "text" name = "usuario" required />
-    </div>
-    </div>
-    </br>
-    <div class="row row-centered">        
-    <div class="col-md-4 col-centered">
-    <label for = "senha">Senha:</label>
-    <input class="form-control" type = "password" name = "senha" required />
-    </div>
+    <div class="row row-center">
+      <div class="col-md-4 col-center">
+      <label for = "usuario">Usuário:</label>
+      <input class="form-control" type = "text" name = "usuario" required />
+      </div>
+      </br>
+      <div class="col-md-4 col-center">
+      <label for = "senha">Senha:</label>
+      <input class="form-control" type = "password" name = "senha" required />
+      </div>
     </div>
     </br>
-    <input type = "submit" class="btn btn-primary" name = "submit_button" value = "Acessar" />
+    <div class="row row-center">
+      <input type = "submit" class="btn btn-primary" name = "submit_button" value = "Acessar" />
+    </div>
     </div>
   </form>
 </div>
+
 <?php include(FOOTER_TEMPLATE); ?>
