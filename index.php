ea55b53016a5d00380653a5d830d1e2e945d5c3f
Lucas R <spallacety@hotmail.com>
2017-03-30 20:10:33 -0300

    Alterações layout páginas

--- index.php
+++ index.php
@@ -4,24 +4,26 @@
 <?php include(HEADER_TEMPLATE); ?>
 <?php $db = open_database(); ?>
 
-<h2>Sistema de Acompanhamento ao Diagnóstico de Autismo</h2>
-<hr/>
+<div class="row row-center">
+  <h2>Sistema de Acompanhamento ao Diagnóstico de Autismo</h2>
+  <hr/>
+</div>
 <div id="form">
   <form action="home.php" method="post">
     <div class="row row-center">
       <div class="col-md-4 col-center">
       <label for = "usuario">Usuário:</label>
-      <input class="form-control" type = "text" name = "usuario" required />
+      <input class="form-control" type="text" name="usuario" required/>
       </div>
       </br>
       <div class="col-md-4 col-center">
       <label for = "senha">Senha:</label>
-      <input class="form-control" type = "password" name = "senha" required />
+      <input class="form-control" type="password" name="senha" required/>
       </div>
     </div>
     </br>
     <div class="row row-center">
-      <input type = "submit" class="btn btn-primary" name = "submit_button" value = "Acessar" />
+      <input type="submit" class="btn btn-primary" name="submit_button" value="Acessar"/>
     </div>
     </div>
   </form>
