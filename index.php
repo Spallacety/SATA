963dfe8e1aded5073750f679d1be7ab2df5c7e32
Spallacety <spallacety@hotmail.com>
2017-03-31 16:18:54 -0300

    Adicionando materialize

--- index.php
+++ index.php
@@ -5,13 +5,14 @@
 <?php $db = open_database(); ?>
 
 <div class="row row-center">
-  <h4 class="main-text-color">Sistema de Acompanhamento ao Diagnóstico de Autismo</h4>
+  <h4 class="main-text-color center">Sistema de Acompanhamento ao Diagnóstico de Autismo</h4>
   <hr/>
 </div>
 <div id="form">
   <form action="home.php" method="post">
     <div class="row center">
-      <div class="col s6 col-center">
+      <div class="col s2 m4"></div>
+      <div class="col s8 m4">
         <div class="input-field">
           <input id="usuario" type="text">
           <label for="usuario">Usuário</label>
@@ -22,6 +23,7 @@
           <label for="senha">Matrícula</label>
         </div>
       </div>
+      <div class="col s2 m4"></div>
     </div>
     </br>
     <div class="row center">
