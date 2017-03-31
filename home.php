9877b4bf7332a4c4a7cb6b0fd14b5adaecae4b48
Spallacety <spallacety@hotmail.com>
2017-03-31 16:49:08 -0300

    Adicionando materialize

--- home.php
+++ home.php
@@ -121,16 +121,10 @@
 			include(HEADER_TEMPLATE);
 			
 			echo  "<br/>";
-			echo  "<br/>";
-						
 			echo "<div class='row center'><p class='center'>Você não tem permissão para visualizar esta página.</p></div>
-			<br/>
-			<br/>
 			<div class='row center'>
 			    <a class='btn waves-effect waves-light main-color' href='index.php'>Fazer login</a>
       		</div>";
-			
-			echo  "<br/>";
 			echo  "<br/>";
 			
 			include(FOOTER_TEMPLATE);
