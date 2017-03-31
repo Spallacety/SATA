a1ce263f8ba014130ddf43151bdf89557310ef70
Spallacety <spallacety@hotmail.com>
2017-03-31 17:24:26 -0300

    Adicionando materialize

--- inc/header.php
+++ inc/header.php
@@ -22,35 +22,34 @@
             <li><a class="dropdown-button secondary-text" href="#!" data-activates="dropdownResultados">Resultados<i class="material-icons right">arrow_drop_down</i></a></li>
             <li><a class="secondary-text" href="#!" onClick="abrirModalSobre();">Sobre</a></li>
           </ul>
-
-          <ul id="dropdownCadastros" class="dropdown-content">
-            <li><a href="../cadastros/pacientes/index.php">Gerenciar Pacientes</a></li>
-            <li><a href="../cadastros/profissionais/index.php">Gerenciar Profissionais</a></li>
-            <li><a href="../cadastros/profissoes/index.php">Gerenciar Profissões</a></li>
-            <li><a href="../cadastros/instituicoes/index.php">Gerenciar Instituições</a></li>
-          </ul>
-
-          <ul id="dropdownAvaliacoes" class="dropdown-content">
-            <li><a href="../avaliacao/avaliacao.php">CARS</a></li>
-            <li><a href="../avaliacao/relatorios/index.php">Avaliação Descritiva</a></li>
-          </ul>
-
-          <ul id="dropdownResultados" class="dropdown-content">
-            <li><a href="../cadastros/pacientes/index.php">Profissionais</a></li>
-          </ul>
-
-          <div id="modal-sobre" class="modal">
-            <div class="modal-content">
-              <h4 class="black-text center">Sobre SADA</h4>
-              <p class="black-text center">SADA é a sigla de Sistema de Apoio ao Diagnóstico de Autismo, desenvolvido no LIMS.
-                Desenvolvido por: Guilherme, Emanuel, Iara, Luana, Lucas Rodrigues e Fernando Santana (Professor Responsável)</p>
-            </div>
-            <div class="modal-footer">
-              <a href="#!" class="modal-action modal-close waves-effect waves-light btn-flat black-text">Voltar</a>
-            </div>
-          </div>
         </div>
 
+        <ul id="dropdownCadastros" class="dropdown-content">
+          <li><a href="../cadastros/pacientes/index.php">Gerenciar Pacientes</a></li>
+          <li><a href="../cadastros/profissionais/index.php">Gerenciar Profissionais</a></li>
+          <li><a href="../cadastros/profissoes/index.php">Gerenciar Profissões</a></li>
+          <li><a href="../cadastros/instituicoes/index.php">Gerenciar Instituições</a></li>
+        </ul>
+
+        <ul id="dropdownAvaliacoes" class="dropdown-content">
+          <li><a href="../avaliacao/avaliacao.php">CARS</a></li>
+          <li><a href="../avaliacao/relatorios/index.php">Avaliação Descritiva</a></li>
+        </ul>
+
+        <ul id="dropdownResultados" class="dropdown-content">
+          <li><a href="../cadastros/pacientes/index.php">Profissionais</a></li>
+        </ul>
+
+        <div id="modal-sobre" class="modal">
+          <div class="modal-content">
+            <h4 class="black-text center">Sobre SADA</h4>
+            <p class="black-text center">SADA é a sigla de Sistema de Apoio ao Diagnóstico de Autismo, desenvolvido no LIMS.
+              Desenvolvido por: Guilherme, Emanuel, Iara, Luana, Lucas Rodrigues e Fernando Santana (Professor Responsável)</p>
+          </div>
+          <div class="modal-footer">
+            <a href="#!" class="modal-action modal-close waves-effect waves-light btn-flat black-text">Voltar</a>
+          </div>
+        </div>
       </nav>
     </div>
 
