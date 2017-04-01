817294d1035a6105fd1da52c5ccbdd0ab343c3a3
Lucas R <spallacety@hotmail.com>
2017-04-01 13:16:15 -0300

    Alteracoes DB

--- config.php
+++ config.php
@@ -2,16 +2,16 @@
 define('DB_NAME', 'epiz_19901727_sada');
 define('DB_USER', 'epiz_19901727');
 define('DB_PASSWORD', '3h0B4p5n');
-define('DB_HOST', 'sql312.epizy.com');
+define('DB_HOST', '185.27.134.10:3306');
 /** caminho absoluto para a pasta do sistema **/
-//if ( !defined('ABSPATH') )
-//	define('ABSPATH', dirname(__FILE__).'/');
+if ( !defined('ABSPATH') )
+	define('ABSPATH', dirname(__FILE__) . '/');
 /** caminho no server para o sistema **/
 if ( !defined('BASEURL') )
 	define('BASEURL', '/sada/');
 /** caminho do arquivo de banco de dados **/
 if ( !defined('DBAPI') )
-	define('DBAPI', '/inc/database.php');
-define('HEADER_TEMPLATE', '/inc/header.php');
-define('FOOTER_TEMPLATE', '/inc/footer.php');
+	define('DBAPI', ABSPATH . 'inc/database.php');
+define('HEADER_TEMPLATE', ABSPATH . 'inc/header.php');
+define('FOOTER_TEMPLATE', ABSPATH . 'inc/footer.php');
 /*$db = mysqli_connect(DB_HOST,DB_USER,DB_PASSWORD,DB_NAME);*/
