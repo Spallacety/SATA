e2a5a175de1377718b7b20225ff89fa0f7783e8b
Lucas R <spallacety@hotmail.com>
2017-04-01 13:56:56 -0300

    Alteracoes DB

--- config.php
+++ config.php
@@ -1,6 +1,6 @@
 <?php
-define('DB_NAME', 'sql1066665');
-define('DB_USER', 'sql1066665');
+define('DB_NAME', 'sql10166665');
+define('DB_USER', 'sql10166665');
 define('DB_PASSWORD', 'guBrvPYJ5Z');
 define('DB_HOST', 'sql10.freemysqlhosting.net:3306');
 /** caminho absoluto para a pasta do sistema **/
