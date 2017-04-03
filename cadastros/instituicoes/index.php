99ac0e7d89b108b149238d6c59fb4e450cb265f9
Lucas R <spallacety@hotmail.com>
2017-04-02 23:41:47 -0300

    Alteracoes páginas

--- cadastros/instituicoes/index.php
+++ cadastros/instituicoes/index.php
@@ -36,7 +36,17 @@
 		<td class="row vallign right-align">
 			<a href="view.php?id=<?php echo $instituicao['id']; ?>" class="btn-flat green-text tooltipped" data-position="bottom" data-delay="50" data-tooltip="Visualizar"><i class="material-icons">visibility</i></a>
 			<a href="edit.php?id=<?php echo $instituicao['id']; ?>" class="btn-flat amber-text tooltipped" data-position="bottom" data-delay="50" data-tooltip="Editar"><i class="material-icons">mode_edit</i></a>
-			<a href="#!" onClick="abrirModalApagarInstituicao();" class="btn-flat red-text tooltipped deleteButton" data-position="bottom" data-delay="50" data-tooltip="Apagar" data-id="<?php echo $instituicao['id']; ?>"><i class="material-icons">delete</i></a>
+			<a href="#modal-apagar-instituicao<?php echo $instituicao['id']; ?>" class="btn-flat red-text tooltipped deleteButton" data-position="bottom" data-delay="50" data-tooltip="Apagar"><i class="material-icons">delete</i></a>
+			<div id="modal-apagar-instituicao<?php echo $instituicao['id']; ?>" class="modal">
+			  <div class="modal-content">
+			    <h4>Excluir</h4>
+			    <p> Deseja mesmo apagar <?php echo $instituicao['descricao']; ?>?</p>
+			  </div>
+			  <div class="modal-footer">
+			    <a href="#!" class="modal-action modal-close btn-flat">Não</a>
+			    <a href="delete.php?id=<?php echo $instituicao['id']; ?>" class="modal-action modal-close btn-flat">Sim</a>
+			  </div>
+			</div>
 		</td>
 	</tr>
 <?php endforeach; ?>
