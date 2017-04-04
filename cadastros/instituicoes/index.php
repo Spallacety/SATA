83fe8bb595811b5c544acab071893145b178c2e9
Lucas R <spallacety@hotmail.com>
2017-04-03 22:27:14 -0300

    Alteracoes páginas

--- cadastros/instituicoes/index.php
+++ cadastros/instituicoes/index.php
@@ -5,13 +5,12 @@
 
 <?php include(HEADER_TEMPLATE); ?>
 
-	<div class="row vallign">
-		<div class="col s12 m6 left-align">
+	<div class="row valign-wrapper">
+		<div class="col s12 m6 valign left-align">
 			<h4 class="main-text">Instituições</h4>
 		</div>
-		<div class="col s12 m6 right-align">
+		<div class="col s12 m6 valign right-align">
 	    <div class="row right">
-	    	<br>
 				<a href="add.php" class="btn white-text blue waves-effect waves-light tooltipped" data-position="bottom" data-delay="50" data-tooltip="Nova instituição"><i class="material-icons">add</i></a>
 				<a href="index.php" class="btn white-text blue waves-effect waves-light tooltipped" data-position="bottom" data-delay="50" data-tooltip="Atualizar"><i class="material-icons">refresh</i></a>
 	  	</div>
@@ -22,9 +21,9 @@
 <table class="highlight">
 <thead>
 	<tr>
-		<th width="10%">Matrícula</th>
-		<th width="63%">Descrição</th>
-		<th width="27%" class"right-align"></th>
+		<th width="5%">Matrícula</th>
+		<th width="45%">Descrição</th>
+		<th width="50%" class"right-align"></th>
 	</tr>
 </thead>
 <tbody>
--- cadastros/instituicoes/modal.php
+++ cadastros/instituicoes/modal.php
@@ -1,10 +1,10 @@
 <div id="modal-apagar-instituicao" class="modal">
   <div class="modal-content">
-    <h4><i class="material-icons md-36 red-text">warning</i> Apagar</h4>
-    <p> Deseja mesmo apagar <a class="red-text" id="itemApagar">*delete*</a>?</p>
+    <h4 class="center"><i class="material-icons md-36 red-text">warning</i> Apagar</h4>
+    <p class="center"> Deseja mesmo apagar <a class="red-text" id="itemApagar">*delete*</a>?</p>
   </div>
   <div class="modal-footer">
-    <a href="#!" class="modal-action modal-close btn-flat">Cancelar</a>
-    <a href="#!" id="deleteBtn" class="modal-action modal-close btn-flat red-text">Apagar</a>
+    <a href="#!" class="modal-action modal-close btn-flat center-align">Cancelar</a>
+    <a href="#!" id="deleteBtn" class="modal-action modal-close btn-flat center-align red-text">Apagar</a>
   </div>
 </div>
