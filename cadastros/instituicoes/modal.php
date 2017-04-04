4ba8faf89b60b4f38b59ef4e58726e2bc18eba0a
Lucas R <spallacety@hotmail.com>
2017-04-03 21:31:49 -0300

    Alteracoes páginas

--- cadastros/instituicoes/modal.php
+++ cadastros/instituicoes/modal.php
@@ -1,7 +1,7 @@
 <div id="modal-apagar-instituicao" class="modal">
   <div class="modal-content">
     <h4>Excluir</h4>
-    <p> Deseja mesmo apagar <input id="itemApagar" value=""></input>?</p>
+    <p> Deseja mesmo apagar <a id="itemApagar" value="123"></a>?</p>
   </div>
   <div class="modal-footer">
     <a href="#!" class="modal-action modal-close btn-flat">Não</a>
