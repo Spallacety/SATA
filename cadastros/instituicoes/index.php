613311bf9e60a4de1edb5c00ae9b8c3de1188fcf
Lucas R <spallacety@hotmail.com>
2017-04-03 22:55:13 -0300

    Alteracoes páginas

--- cadastros/instituicoes/index.php
+++ cadastros/instituicoes/index.php
@@ -12,8 +12,8 @@
 		<div class="col s12 m6 valign right-align">
 	    <div class="row right">
 	    	<br>
-				<a href="add.php" class="btn-floating white-text blue waves-effect waves-light tooltipped" data-position="bottom" data-delay="50" data-tooltip="Nova instituição"><i class="material-icons">add</i></a>
-				<a href="index.php" class="btn-floating white-text blue waves-effect waves-light tooltipped" data-position="bottom" data-delay="50" data-tooltip="Atualizar"><i class="material-icons">refresh</i></a>
+				<a href="add.php" class="btn-floating btn-large white-text blue waves-effect waves-light tooltipped" data-position="bottom" data-delay="50" data-tooltip="Nova instituição"><i class="material-icons">add</i></a>
+				<a href="index.php" class="btn-floating btn-large white-text blue waves-effect waves-light tooltipped" data-position="bottom" data-delay="50" data-tooltip="Atualizar"><i class="material-icons">refresh</i></a>
 	  	</div>
   	</div>
 	</div>
@@ -34,9 +34,7 @@
 		<td><?php echo $instituicao['id']; ?></td>
 		<td><?php echo $instituicao['descricao']; ?></td>		
 		<td class="row vallign right-align">
-			<a href="view.php?id=<?php echo $instituicao['id']; ?>" class="btn-floating tooltipped transparent" data-position="bottom" data-delay="50" data-tooltip="Visualizar"><i class="material-icons green-text">visibility</i></a>
-			<a href="edit.php?id=<?php echo $instituicao['id']; ?>" class="btn-floating tooltipped transparent" data-position="bottom" data-delay="50" data-tooltip="Editar"><i class="material-icons amber-text">mode_edit</i></a>
-			<a href="#!" onClick="abrirModalApagarInstituicao(<?php echo $instituicao['id']; ?>, '<?php echo $instituicao['descricao']; ?>');" class="btn-floating tooltipped transparent" data-position="bottom" data-delay="50" data-tooltip="Apagar"><i class="material-icons red-text">delete</i></a>
+			<a href="view.php?id=<?php echo $instituicao['id']; ?>" class="my-btn tooltipped" data-position="bottom" data-delay="50" data-tooltip="Visualizar"><i class="material-icons green-text">visibility</i></a><a href="edit.php?id=<?php echo $instituicao['id']; ?>" class="my-btn tooltipped" data-position="bottom" data-delay="50" data-tooltip="Editar"><i class="material-icons amber-text">mode_edit</i></a><a href="#!" onClick="abrirModalApagarInstituicao(<?php echo $instituicao['id']; ?>, '<?php echo $instituicao['descricao']; ?>');" class="btn-floating tooltipped transparent" data-position="bottom" data-delay="50" data-tooltip="Apagar"><i class="material-icons red-text">delete</i></a>
 		</td>
 	</tr>
 <?php endforeach; ?>
