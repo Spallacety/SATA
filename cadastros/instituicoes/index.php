aa53d9dbf114c5697c314d11002fdabab490a8f7
Lucas R <spallacety@hotmail.com>
2017-04-03 22:30:58 -0300

    Alteracoes páginas

--- cadastros/instituicoes/index.php
+++ cadastros/instituicoes/index.php
@@ -11,6 +11,7 @@
 		</div>
 		<div class="col s12 m6 valign right-align">
 	    <div class="row right">
+	    	<br>
 				<a href="add.php" class="btn white-text blue waves-effect waves-light tooltipped" data-position="bottom" data-delay="50" data-tooltip="Nova instituição"><i class="material-icons">add</i></a>
 				<a href="index.php" class="btn white-text blue waves-effect waves-light tooltipped" data-position="bottom" data-delay="50" data-tooltip="Atualizar"><i class="material-icons">refresh</i></a>
 	  	</div>
@@ -33,9 +34,9 @@
 		<td><?php echo $instituicao['id']; ?></td>
 		<td><?php echo $instituicao['descricao']; ?></td>		
 		<td class="row vallign right-align">
-			<a href="view.php?id=<?php echo $instituicao['id']; ?>" class="btn-flat green-text tooltipped" data-position="bottom" data-delay="50" data-tooltip="Visualizar"><i class="material-icons">visibility</i></a>
-			<a href="edit.php?id=<?php echo $instituicao['id']; ?>" class="btn-flat amber-text tooltipped" data-position="bottom" data-delay="50" data-tooltip="Editar"><i class="material-icons">mode_edit</i></a>
-			<a href="#!" onClick="abrirModalApagarInstituicao(<?php echo $instituicao['id']; ?>, '<?php echo $instituicao['descricao']; ?>');" class="btn-flat red-text tooltipped" data-position="bottom" data-delay="50" data-tooltip="Apagar"><i class="material-icons">delete</i></a>
+			<a href="view.php?id=<?php echo $instituicao['id']; ?>" class="btn-flat green-text tooltipped img-button" data-position="bottom" data-delay="50" data-tooltip="Visualizar"><i class="material-icons">visibility</i></a>
+			<a href="edit.php?id=<?php echo $instituicao['id']; ?>" class="btn-flat amber-text tooltipped img-button" data-position="bottom" data-delay="50" data-tooltip="Editar"><i class="material-icons">mode_edit</i></a>
+			<a href="#!" onClick="abrirModalApagarInstituicao(<?php echo $instituicao['id']; ?>, '<?php echo $instituicao['descricao']; ?>');" class="btn-flat red-text tooltipped img-button" data-position="bottom" data-delay="50" data-tooltip="Apagar"><i class="material-icons">delete</i></a>
 		</td>
 	</tr>
 <?php endforeach; ?>
