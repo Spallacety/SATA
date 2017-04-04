c1e0544b745b1634f85c8ce6a2ea2e2f281357cc
Lucas R <spallacety@hotmail.com>
2017-04-03 22:10:37 -0300

    Alteracoes páginas

--- cadastros/instituicoes/modal.php
+++ cadastros/instituicoes/modal.php
@@ -1,7 +1,7 @@
 <div id="modal-apagar-instituicao" class="modal">
   <div class="modal-content">
-    <h4><i class="material-icons red-text">warning</i> Apagar</h4>
-    <p class="black-text"> Deseja mesmo apagar <a id="itemApagar">*delete*</a>?</p>
+    <h4><i class="material-icons md-24 red-text">warning</i> Apagar</h4>
+    <p> Deseja mesmo apagar <a class="red-text" id="itemApagar">*delete*</a>?</p>
   </div>
   <div class="modal-footer">
     <a href="#!" class="modal-action modal-close btn-flat">Não</a>
