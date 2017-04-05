ae6e97cdf4451ad074148c45d22b4792b8082f94
Spallacety <spallacety@hotmail.com>
2017-04-05 14:01:17 -0300

    Alteracoes paginas

--- cadastros/instituicoes/add.php
+++ cadastros/instituicoes/add.php
@@ -56,66 +56,83 @@ function cnpj(v){
 
 <?php include(HEADER_TEMPLATE); ?>
 
-<h2>Nova Instituição</h2>
+<h4 class="main-text center">Nova Instituição</h2>
 
 <form action="add.php" method="post">
   <!-- area de campos do form -->
-  <hr/>
-  <div class="container">
-    <div class="row">
-      <div class="col s12 m8 l8">
+  <hr>
+  <div class="row">
+    <div class="col s12 m8 l8">
+      <div class="input-field">
         <input type="text" id="descricao" name="instituicao['descricao']" required>
         <label for="descricao">Descrição</label>
       </div>
+    </div>
 
-      <div class="col s12 m4 l4">
+    <div class="col s12 m4 l4">
+      <div class="input-field">
         <input type="text" onkeypress="mascara(this,cnpj)" maxlength="18" id="cnpj" name="instituicao['cnpj']" required>
         <label for="cnpj">CNPJ</label>
-      </div>    
-    </div>
-    
-    <div class="row">
-      <div class="col s12 m6 l6">
+      </div>
+    </div>    
+  </div>
+  
+  <div class="row">
+    <div class="col s12 m6 l6">
+      <div class="input-field">
         <input type="text" id="endereco" name="instituicao['endereco']" required>
         <label for="endereco">Endereço</label>
       </div>
+    </div>
 
-      <div class="col s12 m4 l4">
+    <div class="col s12 m4 l4">
+      <div class="input-field">
         <input type="text" id="bairro" name="instituicao['bairro']" required>
         <label for="bairro">Bairro</label>
       </div>
-      
-      <div class="col s12 m2 l2">
+    </div>
+    
+    <div class="col s12 m2 l2">
+      <div class="input-field">
         <input type="text" onkeypress="mascara(this,cep)" maxlength="9" id="cep" name="instituicao['cep']" required>
         <label for="cep">CEP</label>
       </div>
     </div>
-    
-    <div class="row">
-      <div class="col s12 m4 l4">
+  </div>
+  
+  <div class="row">
+    <div class="col s12 m4 l4">
+      <div class="input-field">
         <input type="text" id="cidade" name="instituicao['cidade']" required>
         <label for="cidade">Cidade</label>
       </div>
-      
-      <div class="col s3 m1 l1">
+    </div>
+    
+    <div class="col s3 m1 l1">
+      <div class="input-field">
         <input type="text" id="uf" name="instituicao['estado']" required>
         <label for="uf">UF</label>
       </div>
+    </div>
 
-      <div class="col s9 m3 l3">
+    <div class="col s9 m3 l3">
+      <div class="input-field">
         <input type="text" onkeypress="mascara(this,telefone)" maxlength="14" id="telefone" name="instituicao['telefone']" required>
         <label for="telefone">Telefone</label>
       </div>
-      
-       <div class="col s12 m4 l4">
+    </div>
+    
+     <div class="col s12 m4 l4">
+      <div class="input-field">
         <input type="text" id="responsavel" name="instituicao['responsavel']" required>
         <label for="responsavel">Responsável</label>
-      </div>        
-    </div>
+      </div>
+    </div>        
   </div>
     
   <div class="container">
     <div class="row center">
+      <br>
       <button type="submit" class="btn green white-text btn-primary">Salvar</button>
       <a href="index.php" class="btn red white-text btn-default">Cancelar</a>
     </div>
