c428504e1b2eb3bfb29aa330b8179b622af5db77
Lucas R <spallacety@hotmail.com>
2017-04-06 01:11:22 -0300

    Alteracoes páginas

--- home.php
+++ home.php
@@ -127,6 +127,7 @@
 			<div class='row center'>
 			    <a class='btn waves-effect waves-light main-color' href='index.php'>Fazer login</a>
       		</div>";
+      echo  "<br/>";
 			
 			include(FOOTER_TEMPLATE);
 			
--- inc/header.php
+++ inc/header.php
@@ -15,7 +15,7 @@
   <nav class="main-color" role="navigation">
     <div class="nav-wrapper container">
       <a href="https://<?php echo $_SERVER['SERVER_NAME']; ?>/home.php" class="brand-logo secondary-text">SADA</a>
-      <a href="#" data-activates="mobile-menu" class="button-collapse"><i class="material-icons">menu</i></a>
+      <a href="#" data-activates="mobile-menu" class="button-collapse secondary-text"><i class="material-icons">menu</i></a>
       <ul id="nav-mobile" class="right hide-on-med-and-down">
         <li><a class="dropdown-button secondary-text" href="#!" data-activates="dropdownCadastros">Cadastros<i class="material-icons right">arrow_drop_down</i></a></li>
         <li><a class="dropdown-button secondary-text" href="#!" data-activates="dropdownAvaliacoes">Avaliações<i class="material-icons right">arrow_drop_down</i></a></li>
@@ -24,13 +24,13 @@
         <li><a class="secondary-text" href="#!" onClick="abrirModalSobre();">Sobre</a></li>
       </ul>
       <ul class="side-nav" id="mobile-menu">
-        <li><a href="https://<?php echo $_SERVER['SERVER_NAME']; ?>/cadastros/pacientes/index.php" class="main-text">Gerenciar Pacientes</a></li>
-        <li><a href="https://<?php echo $_SERVER['SERVER_NAME']; ?>/cadastros/profissionais/index.php" class="main-text">Gerenciar Profissionais</a></li>
-        <li><a href="https://<?php echo $_SERVER['SERVER_NAME']; ?>/cadastros/profissoes/index.php" class="main-text">Gerenciar Profissões</a></li>
-        <li><a href="https://<?php echo $_SERVER['SERVER_NAME']; ?>/cadastros/instituicoes/index.php" class="main-text">Gerenciar Instituições</a></li>
-        <li><a href="https://<?php echo $_SERVER['SERVER_NAME']; ?>/avaliacao/cars/index.php" class="main-text">CARS</a></li>
-        <li><a href="https://<?php echo $_SERVER['SERVER_NAME']; ?>/avaliacao/cars2/index.php" class="main-text">CARS2</a></li>
-        <li><a href="https://<?php echo $_SERVER['SERVER_NAME']; ?>/avaliacao/relatorios/index.php" class="main-text">Profissionais</a></li>
+        <li><a href="https://<?php echo $_SERVER['SERVER_NAME']; ?>/cadastros/pacientes/index.php">Gerenciar Pacientes</a></li>
+        <li><a href="https://<?php echo $_SERVER['SERVER_NAME']; ?>/cadastros/profissionais/index.php">Gerenciar Profissionais</a></li>
+        <li><a href="https://<?php echo $_SERVER['SERVER_NAME']; ?>/cadastros/profissoes/index.php">Gerenciar Profissões</a></li>
+        <li><a href="https://<?php echo $_SERVER['SERVER_NAME']; ?>/cadastros/instituicoes/index.php">Gerenciar Instituições</a></li>
+        <li><a href="https://<?php echo $_SERVER['SERVER_NAME']; ?>/avaliacao/cars/index.php">CARS</a></li>
+        <li><a href="https://<?php echo $_SERVER['SERVER_NAME']; ?>/avaliacao/cars2/index.php">CARS2</a></li>
+        <li><a href="https://<?php echo $_SERVER['SERVER_NAME']; ?>/avaliacao/relatorios/index.php">Profissionais</a></li>
       </ul>
     </div>
 
