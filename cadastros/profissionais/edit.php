7bfb4ca51de602a949a74cb5d6bb16145394f5a4
Lucas R <spallacety@hotmail.com>
2017-04-05 21:27:24 -0300

    Alteracoes páginas

--- cadastros/profissionais/edit.php
+++ cadastros/profissionais/edit.php
@@ -3,118 +3,91 @@
   edit();
 ?>
 
-<script>
-
-function mascara(o,f){
-    v_obj=o
-    v_fun=f
-    setTimeout("execmascara()",1)
-}
-
-function execmascara(){
-    v_obj.value=v_fun(v_obj.value)
-}
-
-function leech(v){
-    v=v.replace(/o/gi,"0")
-    v=v.replace(/i/gi,"1")
-    v=v.replace(/z/gi,"2")
-    v=v.replace(/e/gi,"3")
-    v=v.replace(/a/gi,"4")
-    v=v.replace(/s/gi,"5")
-    v=v.replace(/t/gi,"7")
-    return v
-}
-
-function soNumeros(v){
-    return v.replace(/\D/g,"")
-}
-
-function telefone(v){
-    v=v.replace(/\D/g,"")                 //Remove tudo o que não é dígito
-    v=v.replace(/^(\d\d)(\d)/g,"($1) $2") //Coloca parênteses em volta dos dois primeiros dígitos
-    v=v.replace(/(\d{4})(\d)/,"$1-$2")    //Coloca hífen entre o quarto e o quinto dígitos
-    return v
-}
-
-function celular(v){
-    v=v.replace(/\D/g,"")                 //Remove tudo o que não é dígito
-    v=v.replace(/^(\d\d)(\d)/g,"($1) $2") //Coloca parênteses em volta dos dois primeiros dígitos
-    v=v.replace(/(\d{5})(\d)/,"$1-$2")    //Coloca hífen entre o quarto e o quinto dígitos
-    return v
-}
-
-function cpf(v){
-    v=v.replace(/\D/g,"")                    //Remove tudo o que não é dígito
-    v=v.replace(/(\d{3})(\d)/,"$1.$2")       //Coloca um ponto entre o terceiro e o quarto dígitos
-    v=v.replace(/(\d{3})(\d)/,"$1.$2")       //Coloca um ponto entre o terceiro e o quarto dígitos
-                                             //de novo (para o segundo bloco de números)
-    v=v.replace(/(\d{3})(\d{1,2})$/,"$1-$2") //Coloca um hífen entre o terceiro e o quarto dígitos
-    return v
-}
-
-</script>
-
 <?php include(HEADER_TEMPLATE); ?>
 
 <h2>Atualizar Profissional</h2>
 
 <form action="edit.php?id=<?php echo $profissional['id']; ?>" method="post">
-  <hr />
   <div class="row">
-    <div class="form-group col-md-6">
-      <label for="name">Nome</label>
-      <input type="text" class="form-control" name="profissional['nome']" value="<?php echo $profissional['nome']; ?>">
+    <div class="col s12 m6 l6">
+      <div class="input-field">
+        <input type="text" id="nome" name="profissional['nome']" value="<?php echo $profissional['nome']; ?>" required>
+        <label for="nome">Nome</label>
+      </div>
     </div>
 
-    <div class="form-group col-md-2">
-      <label for="campo2">CPF</label>
-      <input type="text" onkeypress="mascara(this,cpf)" maxlength="14" class="form-control" name="profissional['cpf']" value="<?php echo $profissional['cpf']; ?>">
+    <div class="col s12 m2 l2">
+      <div class="input-field">
+        <input type="text" class="cpf" maxlength="14" id="cpf" name="profissional['cpf']" value="<?php echo $profissional['cpf']; ?>" required>
+        <label for="cpf">CPF</label>
+      </div>
     </div>
 
-    <div class="form-group col-md-2">
-      <label for="campo2">Tel. Residencial</label>
-      <input type="text" onkeypress="mascara(this,telefone)" maxlength="14" class="form-control" name="profissional['telefone']" value="<?php echo $profissional['telefone']; ?>">
+    <div class="col s12 m2 l2">
+      <div class="input-field">
+        <input type="text" class="telefone" maxlength="14" id="telefone" name="profissional['telefone']" value="<?php echo $profissional['telefone']; ?>" required>
+        <label for="telefone">Telefone</label>
+      </div>
     </div>
 
-   <div class="form-group col-md-2">
-      <label for="campo2">Tel. Celular</label>
-      <input type="text" onkeypress="mascara(this,celular)" maxlength="15" class="form-control" name="profissional['celular']" value="<?php echo $profissional['celular']; ?>">
+    <div class="col s12 m2 l2">
+      <div class="input-field">
+        <input type="text" class="celular" maxlength="15" id="celular" name="profissional['celular']" value="<?php echo $profissional['celular']; ?>" required>
+        <label for="celular">Celular</label>
+      </div>
     </div>
-    
   </div>
-  <div class="row">
 
-<div class="form-group col-md-6">
-      <label for="campo3">Profissão</label>
-      <input type="text" class="form-control" name="profissional['profissao']" value="<?php echo $profissional['profissao']; ?>">
+  <div class="row">   
+    <div class="input-field col s12 m6 l6">
+      <select id="instituicao" name="profissional['id_instituicao']" required>
+        <option></option>
+        <?php if ($instituicoes) : ?>
+        <?php foreach ($instituicoes as $instituicao) : ?>
+          <option <?php if ($profissional['id_instituicao'] == $instituicao['id']) echo ' selected '; ?> value="<?php echo $instituicao['id']; ?>"><?php echo $instituicao['descricao']; ?></option>
+        <?php endforeach; ?>
+        <?php endif; ?>
+      </select>
+      <label for="instituicao">Instituição</label>
     </div>
-    <div class="form-group col-md-6">
-      <label for="campo1">Instituição</label>
-      <input type="text" class="form-control" name="profissional['instituicao']" value="<?php echo $profissional['instituicao']; ?>">
+
+    <div class="input-field col s12 m6 l6">
+      <select id="profissao" name="profissional['id_profissao']" required>
+        <option></option>
+        <?php if ($profissoes) : ?>
+        <?php foreach ($profissoes as $profissao) : ?>
+          <option <?php if ($profissional['id_profissao'] == $profissao['id']) echo ' selected '; ?> value="<?php echo $profissao['id']; ?>"><?php echo $profissao['descricao']; ?></option>
+        <?php endforeach; ?>
+        <?php endif; ?>
+      </select>
+      <label for="profissao">Profissão</label>
     </div>    
   </div>
   
+  <fieldset>
+    <legend class="grey-text"> Para uso no sistema </legend>
+    <div class="row">
+      <div class="col s12 m6 l6">
+        <div class="input-field">
+          <input type="text" id="usuario" name="profissional['usuario']" value="<?php echo $profissional['usuario']; ?>" required>
+          <label for="usuario">Usuário</label>
+        </div>
+      </div>
 
-  <div class="row">
-
-<div class="form-group col-md-4">
-      <label for="campo3">Usuário no Sada</label>
-      <input type="text" class="form-control" name="profissional['usuario']" value="<?php echo $profissional['usuario']; ?>">
+      <div class="col s12 m6 l6">
+        <div class="input-field">
+          <input type="password" id="senha" name="profissional['senha']" value="<?php echo $profissional['senha']; ?>" required>
+          <label for="senha">Senha</label>
+        </div>
+      </div>
     </div>
-    <div class="form-group col-md-4">
-      <label for="campo1">Senha</label>
-      <input type="password" class="form-control" name="profissional['senha']" value="<?php echo $profissional['senha']; ?>">
-    </div>    
-  </div>
+  </fieldset>    
 
-  
-  <div class="row">
-  </div>
-  <div id="actions" class="row">
-    <div class="col-md-12">
-      <button type="submit" class="btn btn-primary">Salvar</button>
-      <a href="index.php" class="btn btn-default">Cancelar</a>
+  <div class="container">
+    <div class="row center">
+      <br>
+      <button type="submit" class="btn green white-text btn-primary">Salvar</button>
+      <a href="index.php" class="btn red white-text btn-default">Cancelar</a>
     </div>
   </div>
 </form>
