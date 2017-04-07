07e634cc03b6bf246017ce115a7e123e94390761
Lucas R <spallacety@hotmail.com>
2017-04-07 01:16:57 -0300

    Alteracoes páginas

--- avaliacao/cars2/avaliacao2.php
+++ avaliacao/cars2/avaliacao2.php
@@ -1,42 +1,52 @@
-<?php require_once '../config.php'; ?>
+<?php require_once '../../config.php'; ?>
 <?php require_once DBAPI; ?>
 
 <?php include(HEADER_TEMPLATE); ?>
 <?php $db = open_database(); ?>
 
+<h4><p align = "center"><b>CARS</b> -  (Escala de Classificação de Autismo na Infância)</p></h4>
+<h5> Paciente: ______</h5>
+<h5> Profissional Responsável: <?php echo $_COOKIE['nome']?></h5>	
+<h5> Data da Avaliação:  <?php $today = date_create('now', new DateTimeZone('America/Sao_Paulo')); echo $today->format("d/m/Y"); ?>;</h5>	
 
-		<form action = "cars.php" method = "post">
-			<fieldset>
-			<h2><p align = "center"><b>CARS</b> -  (Escala de Classificação de Autismo na Infância)</p></h2> 
-			<h3> Paciente: PACIENTE AVALIADO 01</h3>
-			<h3> Profissional Responsável: PROFISSIONAL AVALIADOR 03  </h3>	
-			<h3> Data da Avaliaçãp: DD/MM/AAAA </h3>	
-			<br />		
-			<h3><label for = "p1"><b><u>Pergunta 01 - Relacionamento interpessoal</u></b></h3> 
-			<br />
-			<input type = "radio" name = "p1" value = "1" required/> O comportamento da criança é apropriado para a idade. Alguma timidez, inquietação ou prejuízo pode ser observado, mas não a um nível diferente (atípico) quando comparado com outra de mesma idade. 
-			<br />
-			<input type = "radio" name = "p1" value = "1.5" />
-			 Intermediário 
-			<br />
-			<input type = "radio" name = "p1" value = "2" /> A criança evita olhar o adulto nos olhos; evita o adulto; demonstra dificuldade quando é forçada a tal; é extremamente tímida; não é tão sociável com um adulto quanto uma criança normal de mesma idade; fica agarrada aos familiares de forma mais intensa que outras de mesma idade. 
-			<br />
-			<input type = "radio" name = "p1" value = "2.5" />
-			 Intermediário 
-			<br />
-			<input type = "radio" name = "p1" value = "3" /> A criança às vezes demonstra isolamento. Há necessidade de esforço persistente para obter sua atenção. Há um contato mínimo por iniciativa da criança (o contato pode ser impessoal). 
-			<br />
-			<input type = "radio" name = "p1" value = "3.5" />
-			 Intermediário 
-			<br />
-			<input type = "radio" name = "p1" value = "4" /> A criança é isolada realmente, não se dando conta do que o adulto está fazendo; nunca responde as iniciativas do adulto ou inicia contato. Somente as tentativas muito intensas para obter sua atenção tem algum efeito positivo. 
-			
-			<br />
-			<br />
-			<button type="submit" class="btn btn-primary">Próximo Item</button>			
-						
-			</fieldset>
-		</form>
+<form action = "cars.php" method = "post">
+	<div class="row">
+		<h5 class="main-text center">Relacionamento interpessoal (Questão 1/15)</h5> 
+		<br>
+		<p>
+      <input class="with-gap" name="q1" type="radio" id="q1-1" value="1" required/>
+      <label for="q1-1">O comportamento da criança é apropriado para a idade. Alguma timidez, inquietação ou prejuízo pode ser observado, mas não a um nível diferente (atípico) quando comparado com outra de mesma idade.</label>
+    </p>
+    <p>
+      <input class="with-gap" name="q1" type="radio" id="q1-i1" value="1.5" />
+      <label for="q1-i1">Intermediário</label>
+    </p>
+    <p>
+      <input class="with-gap" name="q1" type="radio" id="q1-2" value="2" />
+      <label for="q1-2">A criança evita olhar o adulto nos olhos; evita o adulto; demonstra dificuldade quando é forçada a tal; é extremamente tímida; não é tão sociável com um adulto quanto uma criança normal de mesma idade; fica agarrada aos familiares de forma mais intensa que outras de mesma idade.</label>
+    </p>
+    <p>
+      <input class="with-gap" name="q1" type="radio" id="q1-i2" value="2.5" />
+      <label for="q1-i2">Intermediário</label>
+    </p>
+    <p>
+      <input class="with-gap" name="q1" type="radio" id="q1-3" value="3" />
+      <label for="q1-3">A criança às vezes demonstra isolamento. Há necessidade de esforço persistente para obter sua atenção. Há um contato mínimo por iniciativa da criança (o contato pode ser impessoal).</label>
+    </p>
+    <p>
+      <input class="with-gap" name="q1" type="radio" id="q1-i3" value="3.5" />
+      <label for="q1-i3">Intermediário</label>
+    </p>
+    <p>
+      <input class="with-gap" name="q1" type="radio" id="q1-4" value="4" />
+      <label for="q1-4">A criança é isolada realmente, não se dando conta do que o adulto está fazendo; nunca responde as iniciativas do adulto ou inicia contato. Somente as tentativas muito intensas para obter sua atenção tem algum efeito positivo.</label>
+    </p>
+  </div>
+	
+	<br />
+	<br />
+	<button type="submit" class="btn btn-primary">Próximo Item</button>
+</form>
 	
 
 <?php include(FOOTER_TEMPLATE); ?>
