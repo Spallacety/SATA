02e4c8a13488a95a805c05dad68d6a0057a8c8da
Spallacety <spallacety@hotmail.com>
2017-04-07 15:53:41 -0300

    Alteracoes paginas

--- config.php
+++ config.php
@@ -2,7 +2,7 @@
 define('DB_NAME', 'epiz_19901727_sadalims');
 define('DB_USER', 'epiz_19901727');
 define('DB_PASSWORD', '3h0B4p5n');
-define('DB_HOST', 'sql312.byetcluster.com:3306');
+define('DB_HOST', '192.168.0.6:58931');
 /** caminho absoluto para a pasta do sistema **/
 if ( !defined('ABSPATH') )
 	define('ABSPATH', dirname(__FILE__) . '/');
