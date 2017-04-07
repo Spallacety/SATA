94a34c695f8c079a8012f77cd1629fa65341fb55
Lucas R <spallacety@hotmail.com>
2017-04-06 21:26:41 -0300

    Alteracoes páginas

--- home.php
+++ home.php
@@ -4,65 +4,11 @@
 
 <?php include(HEADER_TEMPLATE); ?>
 
-<h1>Sistema de Apoio ao Diagnóstico de Autismo</h1>
-<h4>olá, <?php echo $login_cookie = $_COOKIE['nome']?>! id=<?php echo $login_cookie = $_COOKIE['id_profissional']?></h4>
-<hr />
-<div class='row'>
-	<div class='col-xs-6 col-sm-3 col-md-2'>
-		<a href='cadastros/pacientes/add.php' class='btn btn-primary'>
-			<div class='row'>
-				<div class='col-xs-12 text-center'>
-					<i class='fa fa-plus fa-5x'></i>
-				</div>
-				<div class='col-xs-12 text-center'>
-					<p>Novo Paciente</p>
-				</div>
-			</div>
-		</a>
-	</div>
-
-	<div class='col-xs-6 col-sm-3 col-md-2'>
-		<a href='cadastros/pacientes/index.php' class='btn btn-default'>
-			<div class='row'>
-				<div class='col-xs-12 text-center'>
-					<i class='fa fa-user fa-5x'></i>
-				</div>
-				<div class='col-xs-12 text-center'>
-					<p>Pacientes</p>
-				</div>
-			</div>
-		</a>
-	</div>
+<div class="row center">
+  <h4 class="main-text center">Dashboard</h4>
+  <hr>
 </div>
-
-</br>
-
-<div class='row'>
-	<div class='col-xs-6 col-sm-3 col-md-2'>
-		<a href='cadastros/profissionais/add.php' class='btn btn-primary'>
-			<div class='row'>
-				<div class='col-xs-12 text-center'>
-					<i class='fa fa-plus fa-5x'></i>
-				</div>
-				<div class='col-xs-12 text-center'>
-					<p>Novo Profissional</p>
-				</div>
-			</div>
-		</a>
-	</div>
-
-	<div class='col-xs-6 col-sm-3 col-md-2'>
-		<a href='cadastros/profissionais/index.php' class='btn btn-default'>
-			<div class='row'>
-				<div class='col-xs-12 text-center'>
-					<i class='fa fa-user fa-5x'></i>
-				</div>
-				<div class='col-xs-12 text-center'>
-					<p>Profissionais</p>
-				</div>
-			</div>
-		</a>
-	</div>
-</div>
+<p class="center">Usuário logado: <?php echo $login_cookie = $_COOKIE['nome']?>
+	ID no BD: <?php echo $login_cookie = $_COOKIE['id_profissional']?></p>
 
 <?php include(FOOTER_TEMPLATE); ?>
