6bb15e21da0ef28e6f8e4dd503b5d280f7d654b5
Lucas R <spallacety@hotmail.com>
2017-04-08 01:51:06 -0300

    Alteracoes páginas

--- avaliacao/cars2/functions.php
+++ avaliacao/cars2/functions.php
@@ -14,7 +14,7 @@ function add() {
   if (!empty($_POST['q15'])) {
     
     $today = date_create('now', new DateTimeZone('America/Sao_Paulo'));
-    $resultado['id_paciente'] = 1;
+    $resultado['id_paciente'] = '2';
     $resultado['id_profissional'] = $_COOKIE['id_profissional'];
     $resultado['q1'] = $_POST['q1'];
     $resultado['q2'] = $_POST['q2'];
@@ -31,7 +31,7 @@ function add() {
     $resultado['q13'] = $_POST['q13'];
     $resultado['q14'] = $_POST['q14'];
     $resultado['q15'] = $_POST['q15'];
-    $resultado['resultado'] = 15;
+    $resultado['resultado'] = $_POST['q1'] + $_POST['q2'] + $_POST['q3'] + $_POST['q4'] + $_POST['q5'] + $_POST['q6'] + $_POST['q7'] + $_POST['q8'] + $_POST['q9'] + $_POST['q10'] + $_POST['q11'] + $_POST['q12'] + $_POST['q13'] + $_POST['q14'] + $_POST['q15'];
     $resultado['data'] = $now->format("Y-m-d");
 
     save('cars', $resultado);
