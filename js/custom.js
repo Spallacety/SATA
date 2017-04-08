2648f3b995b0f90485be8edb692f4d0556480981
Lucas R <spallacety@hotmail.com>
2017-04-08 02:05:13 -0300

    Alteracoes páginas

--- js/custom.js
+++ js/custom.js
@@ -24,7 +24,7 @@ $(document).ready(function() {
   $('.celular').mask('(00) 00000-0000');
   $('.cpf').mask('000.000.000-00', {reverse: true});
   $('.cnpj').mask('00.000.000/0000-00');
-  $('.select').select2();
+  Materialize.updateTextFields();
 });
 
 function abrirModalSobre(){
