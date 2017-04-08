6b242194c94a8811023d9461b52535c9791b63d3
Lucas R <spallacety@hotmail.com>
2017-04-07 21:22:12 -0300

    Alteracoes páginas

--- config.php
+++ config.php
@@ -1,17 +1,25 @@
 <?php
-define('DB_NAME', 'dbsada');
-define('DB_USER', 'usersada');
-define('DB_PASSWORD', 'sadaifpi01S');
-define('DB_HOST', 'db4free.net:3306');
+
+$url = parse_url(getenv("CLEARDB_DATABASE_URL"));
+
+$host = $url["host"];
+$username = $url["user"];
+$password = $url["pass"];
+$db = substr($url["path"], 1);
+
+define('DB_NAME', $db);
+define('DB_USER', $username);
+define('DB_PASSWORD', $password);
+define('DB_HOST', $host);
 /** caminho absoluto para a pasta do sistema **/
 if ( !defined('ABSPATH') )
-	define('ABSPATH', dirname(__FILE__) . '/');
+  define('ABSPATH', dirname(__FILE__) . '/');
 /** caminho no server para o sistema **/
 if ( !defined('BASEURL') )
-	define('BASEURL', '/sada/');
+  define('BASEURL', '/sada/');
 /** caminho do arquivo de banco de dados **/
 if ( !defined('DBAPI') )
-	define('DBAPI', ABSPATH . 'inc/database.php');
+  define('DBAPI', ABSPATH . 'inc/database.php');
 
 define('HEADER_TEMPLATE', ABSPATH . 'inc/header.php');
 define('FOOTER_TEMPLATE', ABSPATH . 'inc/footer.php');
