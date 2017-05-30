1484bb0eaf5e5417463d4e4b8be03223e8041b6c
Spallacety <spallacety@hotmail.com>
2017-05-30 13:33:47 -0300

    commit

--- avaliacao/cars/functions.php
+++ avaliacao/cars/functions.php
@@ -17,34 +17,30 @@ function initAttr(){
 }
 
 function add() {
-  if (isset($_GET['id'])) {
-    if (!empty($_POST['q15'])) {
-      
-      $today = date_create('now', new DateTimeZone('America/Sao_Paulo'));
-      $resultado['id_paciente'] = $_GET['id'];
-      $resultado['id_profissional'] = $_COOKIE['id_profissional'];
-      $resultado['q1'] = $_POST['q1'];
-      $resultado['q2'] = $_POST['q2'];
-      $resultado['q3'] = $_POST['q3'];
-      $resultado['q4'] = $_POST['q4'];
-      $resultado['q5'] = $_POST['q5'];
-      $resultado['q6'] = $_POST['q6'];
-      $resultado['q7'] = $_POST['q7'];
-      $resultado['q8'] = $_POST['q8'];
-      $resultado['q9'] = $_POST['q9'];
-      $resultado['q10'] = $_POST['q10'];
-      $resultado['q11'] = $_POST['q11'];
-      $resultado['q12'] = $_POST['q12'];
-      $resultado['q13'] = $_POST['q13'];
-      $resultado['q14'] = $_POST['q14'];
-      $resultado['q15'] = $_POST['q15'];
-      $resultado['resultado'] = $_POST['q1'] + $_POST['q2'] + $_POST['q3'] + $_POST['q4'] + $_POST['q5'] + $_POST['q6'] + $_POST['q7'] + $_POST['q8'] + $_POST['q9'] + $_POST['q10'] + $_POST['q11'] + $_POST['q12'] + $_POST['q13'] + $_POST['q14'] + $_POST['q15'];
-      $resultado['criacao'] = $today->format("Y-m-d H:i:s");
+  if (!empty($_POST['q15'])) {
+    
+    $today = date_create('now', new DateTimeZone('America/Sao_Paulo'));
+    $resultado['id_paciente'] = $_GET['id'];
+    $resultado['id_profissional'] = $_COOKIE['id_profissional'];
+    $resultado['q1'] = $_POST['q1'];
+    $resultado['q2'] = $_POST['q2'];
+    $resultado['q3'] = $_POST['q3'];
+    $resultado['q4'] = $_POST['q4'];
+    $resultado['q5'] = $_POST['q5'];
+    $resultado['q6'] = $_POST['q6'];
+    $resultado['q7'] = $_POST['q7'];
+    $resultado['q8'] = $_POST['q8'];
+    $resultado['q9'] = $_POST['q9'];
+    $resultado['q10'] = $_POST['q10'];
+    $resultado['q11'] = $_POST['q11'];
+    $resultado['q12'] = $_POST['q12'];
+    $resultado['q13'] = $_POST['q13'];
+    $resultado['q14'] = $_POST['q14'];
+    $resultado['q15'] = $_POST['q15'];
+    $resultado['resultado'] = $_POST['q1'] + $_POST['q2'] + $_POST['q3'] + $_POST['q4'] + $_POST['q5'] + $_POST['q6'] + $_POST['q7'] + $_POST['q8'] + $_POST['q9'] + $_POST['q10'] + $_POST['q11'] + $_POST['q12'] + $_POST['q13'] + $_POST['q14'] + $_POST['q15'];
+    $resultado['criacao'] = $today->format("Y-m-d H:i:s");
 
-      save('cars', $resultado);
-      header('location: index.php');
-    }
-  } else {
+    save('cars', $resultado);
     header('location: index.php');
   }
 }
