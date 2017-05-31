c9f10acefce0d1e58e76380723f1554133015cde
Spallacety <spallacety@hotmail.com>
2017-05-31 15:25:26 -0300

    commit

--- cadastros/pacientes/functions.php
+++ cadastros/pacientes/functions.php
@@ -26,7 +26,7 @@ function add() {
     $paciente['modificacao'] = $paciente['criacao'] = $today->format("Y-m-d H:i:s");
     
     save('pacientes', $paciente);
-    header('location: /');
+    header('location: /cadastros/pacientes/');
   }
 }
 
@@ -41,13 +41,13 @@ function edit() {
       $paciente = $_POST['paciente'];
       $paciente['modificacao'] = $now->format("Y-m-d H:i:s");
       update('pacientes', $id, $paciente);
-      header('location: /');
+      header('location: /cadastros/pacientes/');
     } else {
       global $paciente;
       $paciente = find('pacientes', $id);
     } 
   } else {
-    header('location: /');
+    header('location: /cadastros/pacientes/');
   }
 }
 
@@ -67,5 +67,5 @@ function view($id = null) {
 function delete($id = null) {
   global $paciente;
   $paciente = remove('pacientes', $id);
-  header('location: /');
+  header('location: /cadastros/pacientes/');
 }
