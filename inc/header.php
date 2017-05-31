78040c66af96a12fd7ffddab4daff265156bf512
Spallacety <spallacety@hotmail.com>
2017-05-31 15:49:05 -0300

    commit

--- inc/header.php
+++ inc/header.php
@@ -18,7 +18,7 @@
       <ul id="nav-mobile" class="right hide-on-med-and-down">
         <li><a class="dropdown-button secondary-text" href="#!" data-activates="dropdownCadastros">Gerenciar<i class="material-icons right">arrow_drop_down</i></a></li>
         <li><a class="dropdown-button secondary-text" href="#!" data-activates="dropdownAvaliacoes">Avaliações<i class="material-icons right">arrow_drop_down</i></a></li>
-        <li><a class="secondary-text" href="#!" onClick="abrirModalSobre();">Sobre</a></li>
+        <li><a class="dropdown-button secondary-text" href="#!" data-activates="dropdownGeral"><i class="material-icons right">arrow_drop_down</i></a></li>
       </ul>
       <ul class="side-nav" id="mobile-menu">
         <li><a href="/cadastros/pacientes/">Gerenciar Pacientes</a></li>
@@ -38,6 +38,11 @@
 
     <ul id="dropdownAvaliacoes" class="dropdown-content">
       <li><a href="/avaliacao/cars/" class="main-text">CARS</a></li>
+    </ul>
+
+    <ul id="dropdownGeral" class="dropdown-content">
+      <li><a onClick="abrirModalSobre();" class="right-allign">Sobre</a></li>
+      <li><a class="red-text"><i class="material-icons">view_module</i>Sair</a></li>
     </ul>
 
     <div id="modal-sobre" class="modal">
