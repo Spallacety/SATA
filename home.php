4465d8396557aa07de0788add07cd7679fb6448c
Spallacety <spallacety@hotmail.com>
2017-06-05 15:13:05 -0300

    commit

--- home.php
+++ home.php
@@ -8,8 +8,7 @@
   <h4 class="main-text center">Dashboard</h4>
   <hr>
 </div>
-<h5 class="center">Usuário logado: <a class="grey-text"><?php echo $_COOKIE['nome']?></a></h5>
-<h5 class="center">ID no BD: <a class="grey-text"><?php echo $_COOKIE['id_profissional']?></a></h5>
+<h5 class="center">Bem vindo, <a class="grey-text"><?php echo $_COOKIE['nome']?></a></h5>
 
 <div class="row center">
   <br>
