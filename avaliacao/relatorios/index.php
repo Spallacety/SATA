37b88a023fb393951a3717b88a1f2f045a01d2cd
Spallacety <spallacety@hotmail.com>
2017-06-06 14:27:32 -0300

    commit

--- avaliacao/relatorios/index.php
+++ avaliacao/relatorios/index.php
@@ -12,19 +12,7 @@
 
 <?php $result = findLast(2); ?>
 
-<script type="text/javascript">
 
-  google.load('visualization', '1.0', {'packages':['corechart']});
-  google.setOnLoadCallback(function(){
-  
-    var json_text = $.ajax({url: "lastResults.json", dataType:"json", async: false}).responseText;
-    var json = eval("(" + json_text + ")");
-    var dados = new google.visualization.DataTable(json.dados);
-
-    var chart = new google.visualization.ColumnChart(document.getElementById('area_grafico'));
-    chart.draw(dados, json.config);
-  }
-</script>
 
 <div id="area_grafico"></div>
 
