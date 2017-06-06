e6787abb5f6f488ec5f8ce1cd8837804172b71de
Spallacety <spallacety@hotmail.com>
2017-06-06 13:27:50 -0300

    commit

--- avaliacao/relatorios/index.php
+++ avaliacao/relatorios/index.php
@@ -10,17 +10,11 @@
 
 <script type="text/javascript">
 
-      // Carregar a API de visualizacao e os pacotes necessarios.
       google.load('visualization', '1.0', {'packages':['corechart']});
 
-      // Especificar um callback para ser executado quando a API for carregada.
       google.setOnLoadCallback(drawLastResults);
 
-      /**
-       * Funcao que preenche os dados do grafico
-       */
       function drawLastResults() {
-        // Montar os dados usados pelo grafico
         var dados = new google.visualization.DataTable();
         dados.addColumn('string', 'Gênero');
         dados.addColumn('number', 'Quantidades');
@@ -29,18 +23,14 @@
           ['Feminino', 20]
         ]);
 
-        // Configuracoes do grafico
         var config = {
             'title':'Quantidade de alunos por gênero',
             'width':400,
             'height':300
         };
 
-        // Instanciar o objeto de geracao de graficos de pizza,
-        // informando o elemento HTML onde o grafico sera desenhado.
-        var chart = new google.visualization.PieChart(document.getElementById('area_grafico'));
+        var chart = new google.visualization.BarChart(document.getElementById('area_grafico'));
 
-        // Desenhar o grafico (usando os dados e as configuracoes criadas)
         chart.draw(dados, config);
       }
     </script>
