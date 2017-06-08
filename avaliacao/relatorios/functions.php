2118aa3fdaf5bd3a2506f0fdf13197156cf613b0
Spallacety <spallacety@hotmail.com>
2017-06-08 14:00:30 -0300

    commit

--- avaliacao/relatorios/functions.php
+++ avaliacao/relatorios/functions.php
@@ -9,7 +9,7 @@ $relatorio = null;
  */
 function index() {
 	global $relatorios;
-	$relatorios = find_all('avaliacoes');
+	$relatorios = findLast(2);
 }
 
 /**
@@ -76,7 +76,7 @@ function findLast($id = null){
 
   try {
     if ($id) {
-      $sql = "SELECT * FROM avaliacoes WHERE id_paciente = 2 ORDER BY criacao DESC LIMIT 10";
+      $sql = "SELECT * FROM avaliacoes WHERE id_paciente = " . $id . " ORDER BY modificacao DESC LIMIT 10";
       $result = $database->query($sql);
       
       if ($result->num_rows > 0) {
