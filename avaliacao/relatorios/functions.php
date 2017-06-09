123a0112f7226ee261734cae7924f970b40ff82e
Spallacety <spallacety@hotmail.com>
2017-06-09 16:15:52 -0300

    commit

--- avaliacao/relatorios/functions.php
+++ avaliacao/relatorios/functions.php
@@ -76,7 +76,7 @@ function findLast($id){
 
   try {
     if ($id) {
-      $sql = "SELECT * FROM avaliacoes WHERE id_paciente = " . $id . " ORDER BY modificacao DESC LIMIT 3";
+      $sql = "SELECT * FROM avaliacoes WHERE id_paciente = " . $id . "ORDER BY modificacao DESC LIMIT 10";
       $result = $database->query($sql);
       
       $found = array();
