5070bde8ef7ddcec512d2624629c8c67a21ea97f
Lucas R <spallacety@hotmail.com>
2017-06-09 20:39:02 -0300

    a

--- avaliacao/relatorios/index.php
+++ avaliacao/relatorios/index.php
@@ -8,14 +8,74 @@
 
 <br>
 
-    <script type="text/javascript" src="https://www.google.com/jsapi"></script>
-    <script type="text/javascript">
+<script type="text/javascript" src="https://www.google.com/jsapi"></script>
+<script type="text/javascript">
 
-    drawChartLastResult();
-    
-    </script>
+  google.load('visualization', '1.0', {'packages':['corechart']});
 
-    <div id="area_grafico"></div>
+  google.setOnLoadCallback(desenharGrafico);
+
+  function desenharGrafico() {
+    var dados = new google.visualization.DataTable();
+    dados.addColumn('string', 'Data');
+    dados.addColumn('number', 'Total');
+    dados.addRows([
+      <?php
+        if ($relatorios) :
+          foreach ($relatorios as $relatorio) :
+            echo "['" . $relatorio['modificacao'] . "', " . $relatorio['resultado'] . "],";
+          endforeach;
+        endif;
+      ?>
+    ]);
+
+    var config = {
+        'title':'Ultimas 10 avaliações',
+        'width':700,
+        'height':300,
+        'legend': { position: "none" },
+    };
+
+    var chart = new google.visualization.ColumnChart(document.getElementById('area_grafico'));
+
+    chart.draw(dados, config);
+  }
+</script>
+<script type="text/javascript">
+
+  google.load('visualization', '1.0', {'packages':['corechart']});
+
+  google.setOnLoadCallback(desenharGrafico);
+
+  function desenharGrafico() {
+    var dados = new google.visualization.DataTable();
+    dados.addColumn('string', 'Data');
+    dados.addColumn('number', 'Total');
+    dados.addRows([
+      <?php
+        if ($relatorios) :
+          foreach ($relatorios as $relatorio) :
+            echo "['" . $relatorio['modificacao'] . "', " . $relatorio['q1'] . "],";
+          endforeach;
+        endif;
+      ?>
+    ]);
+
+    var config = {
+        'title':'Ultimas 10 avaliações',
+        'width':700,
+        'height':300,
+        'legend': { position: "none" },
+    };
+
+    var chart = new google.visualization.ColumnChart(document.getElementById('area_grafico2'));
+
+    chart.draw(dados, config);
+  }
+</script>
+
+<div id="area_grafico"></div>
+<div id="area_grafico2"></div>
 
 <table class="highlight">
   <thead>
