33fba4ff2a0b4c828c4dc49cdd8091f1361ba0ff
Spallacety <spallacety@hotmail.com>
2017-06-09 17:21:04 -0300

    commit

--- avaliacao/relatorios/index.php
+++ avaliacao/relatorios/index.php
@@ -6,24 +6,15 @@
 
 <?php include(HEADER_TEMPLATE); ?>
 
+<br>
 
-    <!-- Carregar a API do google -->
     <script type="text/javascript" src="https://www.google.com/jsapi"></script>
-
-    <!-- Preparar a geracao do grafico -->
     <script type="text/javascript">
 
-      // Carregar a API de visualizacao e os pacotes necessarios.
       google.load('visualization', '1.0', {'packages':['corechart']});
 
-      // Especificar um callback para ser executado quando a API for carregada.
       google.setOnLoadCallback(desenharGrafico);
-
-      /**
-       * Funcao que preenche os dados do grafico
-       */
       function desenharGrafico() {
-        // Montar os dados usados pelo grafico
         var dados = new google.visualization.DataTable();
         dados.addColumn('string', 'Gênero');
         dados.addColumn('number', 'Quantidades');
@@ -32,25 +23,19 @@
           ['Feminino', 20]
         ]);
 
-        // Configuracoes do grafico
         var config = {
             'title':'Quantidade de alunos por gênero',
             'width':400,
             'height':300
         };
 
-        // Instanciar o objeto de geracao de graficos de pizza,
-        // informando o elemento HTML onde o grafico sera desenhado.
-        var chart = new google.visualization.PieChart(document.getElementById('area_grafico'));
+        var chart = new google.visualization.ColumnChart(document.getElementById('area_grafico'));
 
-        // Desenhar o grafico (usando os dados e as configuracoes criadas)
         chart.draw(dados, config);
       }
     </script>
-    
-    <div id="area_grafico"></div>
 
-<br>
+    <div id="area_grafico"></div>
 
 <table class="highlight">
   <thead>
