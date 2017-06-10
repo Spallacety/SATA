69b221cc26f69b8df7bb9224db221bd9aa229ff7
Lucas R <spallacety@hotmail.com>
2017-06-09 21:16:40 -0300

    a

--- avaliacao/relatorios/index.php
+++ avaliacao/relatorios/index.php
@@ -31,14 +31,18 @@
     ]);
 
     var config = {
-        title:'Ultimas 10 avaliações',
+        title:'Resultado total',
         width:700,
         height:300,
         legend: { position: "none" },
         colors: ['#1a237e'],
+        vAxis: {
+          minValue: 0, 
+          ticks: [0, 15, 30, 45, 60]
+        },
     };
 
-    var chart = new google.visualization.ColumnChart(document.getElementById('area_grafico'));
+    var chart = new google.visualization.ColumnChart(document.getElementById('grafico_total'));
 
     chart.draw(dados, config);
   }
@@ -71,19 +75,565 @@
         colors: ['#1a237e'],
         vAxis: {
           minValue: 0, 
-          ticks: [0, 1, 2, 3, 4, 5]
+          ticks: [0, 1, 2, 3, 4]
         },
     };
 
-    var chart = new google.visualization.ColumnChart(document.getElementById('area_grafico2'));
+    var chart = new google.visualization.ColumnChart(document.getElementById('grafico_q1'));
+
+    chart.draw(dados, config);
+  }
+</script>
+<script type="text/javascript">
+
+  google.load('visualization', '1.0', {'packages':['corechart']});
+
+  google.setOnLoadCallback(desenharGrafico);
+
+  function desenharGrafico() {
+    var dados = new google.visualization.DataTable();
+    dados.addColumn('string', 'Data');
+    dados.addColumn('number', 'Resposta');
+    dados.addRows([
+      <?php
+        if ($relatorios) :
+          foreach ($relatorios as $relatorio) :
+            echo "['" . $relatorio['modificacao'] . "', " . $relatorio['q2'] . "],";
+          endforeach;
+        endif;
+      ?>
+    ]);
+
+    var config = {
+        title:'Imitação (Questão 2/15)',
+        width:700,
+        height:300,
+        legend: { position: "none" },
+        colors: ['#1a237e'],
+        vAxis: {
+          minValue: 0, 
+          ticks: [0, 1, 2, 3, 4]
+        },
+    };
+
+    var chart = new google.visualization.ColumnChart(document.getElementById('grafico_q2'));
+
+    chart.draw(dados, config);
+  }
+</script>
+<script type="text/javascript">
+
+  google.load('visualization', '1.0', {'packages':['corechart']});
+
+  google.setOnLoadCallback(desenharGrafico);
+
+  function desenharGrafico() {
+    var dados = new google.visualization.DataTable();
+    dados.addColumn('string', 'Data');
+    dados.addColumn('number', 'Resposta');
+    dados.addRows([
+      <?php
+        if ($relatorios) :
+          foreach ($relatorios as $relatorio) :
+            echo "['" . $relatorio['modificacao'] . "', " . $relatorio['q3'] . "],";
+          endforeach;
+        endif;
+      ?>
+    ]);
+
+    var config = {
+        title:'Resposta emocional (Questão 3/15)',
+        width:700,
+        height:300,
+        legend: { position: "none" },
+        colors: ['#1a237e'],
+        vAxis: {
+          minValue: 0, 
+          ticks: [0, 1, 2, 3, 4]
+        },
+    };
+
+    var chart = new google.visualization.ColumnChart(document.getElementById('grafico_q3'));
+
+    chart.draw(dados, config);
+  }
+</script>
+<script type="text/javascript">
+
+  google.load('visualization', '1.0', {'packages':['corechart']});
+
+  google.setOnLoadCallback(desenharGrafico);
+
+  function desenharGrafico() {
+    var dados = new google.visualization.DataTable();
+    dados.addColumn('string', 'Data');
+    dados.addColumn('number', 'Resposta');
+    dados.addRows([
+      <?php
+        if ($relatorios) :
+          foreach ($relatorios as $relatorio) :
+            echo "['" . $relatorio['modificacao'] . "', " . $relatorio['q4'] . "],";
+          endforeach;
+        endif;
+      ?>
+    ]);
+
+    var config = {
+        title:'Expressão corporal (Questão 4/15)',
+        width:700,
+        height:300,
+        legend: { position: "none" },
+        colors: ['#1a237e'],
+        vAxis: {
+          minValue: 0, 
+          ticks: [0, 1, 2, 3, 4]
+        },
+    };
+
+    var chart = new google.visualization.ColumnChart(document.getElementById('grafico_q4'));
+
+    chart.draw(dados, config);
+  }
+</script>
+<script type="text/javascript">
+
+  google.load('visualization', '1.0', {'packages':['corechart']});
+
+  google.setOnLoadCallback(desenharGrafico);
+
+  function desenharGrafico() {
+    var dados = new google.visualization.DataTable();
+    dados.addColumn('string', 'Data');
+    dados.addColumn('number', 'Resposta');
+    dados.addRows([
+      <?php
+        if ($relatorios) :
+          foreach ($relatorios as $relatorio) :
+            echo "['" . $relatorio['modificacao'] . "', " . $relatorio['q5'] . "],";
+          endforeach;
+        endif;
+      ?>
+    ]);
+
+    var config = {
+        title:'Uso do objeto (Questão 5/15)',
+        width:700,
+        height:300,
+        legend: { position: "none" },
+        colors: ['#1a237e'],
+        vAxis: {
+          minValue: 0, 
+          ticks: [0, 1, 2, 3, 4]
+        },
+    };
+
+    var chart = new google.visualization.ColumnChart(document.getElementById('grafico_q5'));
+
+    chart.draw(dados, config);
+  }
+</script>
+<script type="text/javascript">
+
+  google.load('visualization', '1.0', {'packages':['corechart']});
+
+  google.setOnLoadCallback(desenharGrafico);
+
+  function desenharGrafico() {
+    var dados = new google.visualization.DataTable();
+    dados.addColumn('string', 'Data');
+    dados.addColumn('number', 'Resposta');
+    dados.addRows([
+      <?php
+        if ($relatorios) :
+          foreach ($relatorios as $relatorio) :
+            echo "['" . $relatorio['modificacao'] . "', " . $relatorio['q6'] . "],";
+          endforeach;
+        endif;
+      ?>
+    ]);
+
+    var config = {
+        title:'Adaptação a mudanças (Questão 6/15)',
+        width:700,
+        height:300,
+        legend: { position: "none" },
+        colors: ['#1a237e'],
+        vAxis: {
+          minValue: 0, 
+          ticks: [0, 1, 2, 3, 4]
+        },
+    };
+
+    var chart = new google.visualization.ColumnChart(document.getElementById('grafico_q6'));
+
+    chart.draw(dados, config);
+  }
+</script>
+<script type="text/javascript">
+
+  google.load('visualization', '1.0', {'packages':['corechart']});
+
+  google.setOnLoadCallback(desenharGrafico);
+
+  function desenharGrafico() {
+    var dados = new google.visualization.DataTable();
+    dados.addColumn('string', 'Data');
+    dados.addColumn('number', 'Resposta');
+    dados.addRows([
+      <?php
+        if ($relatorios) :
+          foreach ($relatorios as $relatorio) :
+            echo "['" . $relatorio['modificacao'] . "', " . $relatorio['q7'] . "],";
+          endforeach;
+        endif;
+      ?>
+    ]);
+
+    var config = {
+        title:'Uso do olhar (Questão 7/15)',
+        width:700,
+        height:300,
+        legend: { position: "none" },
+        colors: ['#1a237e'],
+        vAxis: {
+          minValue: 0, 
+          ticks: [0, 1, 2, 3, 4]
+        },
+    };
+
+    var chart = new google.visualization.ColumnChart(document.getElementById('grafico_q7'));
+
+    chart.draw(dados, config);
+  }
+</script>
+<script type="text/javascript">
+
+  google.load('visualization', '1.0', {'packages':['corechart']});
+
+  google.setOnLoadCallback(desenharGrafico);
+
+  function desenharGrafico() {
+    var dados = new google.visualization.DataTable();
+    dados.addColumn('string', 'Data');
+    dados.addColumn('number', 'Resposta');
+    dados.addRows([
+      <?php
+        if ($relatorios) :
+          foreach ($relatorios as $relatorio) :
+            echo "['" . $relatorio['modificacao'] . "', " . $relatorio['q8'] . "],";
+          endforeach;
+        endif;
+      ?>
+    ]);
+
+    var config = {
+        title:'Uso da audição (Questão 8/15)',
+        width:700,
+        height:300,
+        legend: { position: "none" },
+        colors: ['#1a237e'],
+        vAxis: {
+          minValue: 0, 
+          ticks: [0, 1, 2, 3, 4]
+        },
+    };
+
+    var chart = new google.visualization.ColumnChart(document.getElementById('grafico_q8'));
+
+    chart.draw(dados, config);
+  }
+</script>
+<script type="text/javascript">
+
+  google.load('visualization', '1.0', {'packages':['corechart']});
+
+  google.setOnLoadCallback(desenharGrafico);
+
+  function desenharGrafico() {
+    var dados = new google.visualization.DataTable();
+    dados.addColumn('string', 'Data');
+    dados.addColumn('number', 'Resposta');
+    dados.addRows([
+      <?php
+        if ($relatorios) :
+          foreach ($relatorios as $relatorio) :
+            echo "['" . $relatorio['modificacao'] . "', " . $relatorio['q9'] . "],";
+          endforeach;
+        endif;
+      ?>
+    ]);
+
+    var config = {
+        title:'Uso do paladar, olfato e do tato (Questão 9/15)',
+        width:700,
+        height:300,
+        legend: { position: "none" },
+        colors: ['#1a237e'],
+        vAxis: {
+          minValue: 0, 
+          ticks: [0, 1, 2, 3, 4]
+        },
+    };
+
+    var chart = new google.visualization.ColumnChart(document.getElementById('grafico_q9'));
+
+    chart.draw(dados, config);
+  }
+</script>
+<script type="text/javascript">
+
+  google.load('visualization', '1.0', {'packages':['corechart']});
+
+  google.setOnLoadCallback(desenharGrafico);
+
+  function desenharGrafico() {
+    var dados = new google.visualization.DataTable();
+    dados.addColumn('string', 'Data');
+    dados.addColumn('number', 'Resposta');
+    dados.addRows([
+      <?php
+        if ($relatorios) :
+          foreach ($relatorios as $relatorio) :
+            echo "['" . $relatorio['modificacao'] . "', " . $relatorio['q10'] . "],";
+          endforeach;
+        endif;
+      ?>
+    ]);
+
+    var config = {
+        title:'Medo e nervosismo (Questão 10/15)',
+        width:700,
+        height:300,
+        legend: { position: "none" },
+        colors: ['#1a237e'],
+        vAxis: {
+          minValue: 0, 
+          ticks: [0, 1, 2, 3, 4]
+        },
+    };
+
+    var chart = new google.visualization.ColumnChart(document.getElementById('grafico_q10'));
+
+    chart.draw(dados, config);
+  }
+</script>
+<script type="text/javascript">
+
+  google.load('visualization', '1.0', {'packages':['corechart']});
+
+  google.setOnLoadCallback(desenharGrafico);
+
+  function desenharGrafico() {
+    var dados = new google.visualization.DataTable();
+    dados.addColumn('string', 'Data');
+    dados.addColumn('number', 'Resposta');
+    dados.addRows([
+      <?php
+        if ($relatorios) :
+          foreach ($relatorios as $relatorio) :
+            echo "['" . $relatorio['modificacao'] . "', " . $relatorio['q11'] . "],";
+          endforeach;
+        endif;
+      ?>
+    ]);
+
+    var config = {
+        title:'Comunicação verbal (Questão 11/15)',
+        width:700,
+        height:300,
+        legend: { position: "none" },
+        colors: ['#1a237e'],
+        vAxis: {
+          minValue: 0, 
+          ticks: [0, 1, 2, 3, 4]
+        },
+    };
+
+    var chart = new google.visualization.ColumnChart(document.getElementById('grafico_q11'));
+
+    chart.draw(dados, config);
+  }
+</script>
+<script type="text/javascript">
+
+  google.load('visualization', '1.0', {'packages':['corechart']});
+
+  google.setOnLoadCallback(desenharGrafico);
+
+  function desenharGrafico() {
+    var dados = new google.visualization.DataTable();
+    dados.addColumn('string', 'Data');
+    dados.addColumn('number', 'Resposta');
+    dados.addRows([
+      <?php
+        if ($relatorios) :
+          foreach ($relatorios as $relatorio) :
+            echo "['" . $relatorio['modificacao'] . "', " . $relatorio['q12'] . "],";
+          endforeach;
+        endif;
+      ?>
+    ]);
+
+    var config = {
+        title:'Comunicação não-verbal (Questão 12/15)',
+        width:700,
+        height:300,
+        legend: { position: "none" },
+        colors: ['#1a237e'],
+        vAxis: {
+          minValue: 0, 
+          ticks: [0, 1, 2, 3, 4]
+        },
+    };
+
+    var chart = new google.visualization.ColumnChart(document.getElementById('grafico_q12'));
+
+    chart.draw(dados, config);
+  }
+</script>
+<script type="text/javascript">
+
+  google.load('visualization', '1.0', {'packages':['corechart']});
+
+  google.setOnLoadCallback(desenharGrafico);
+
+  function desenharGrafico() {
+    var dados = new google.visualization.DataTable();
+    dados.addColumn('string', 'Data');
+    dados.addColumn('number', 'Resposta');
+    dados.addRows([
+      <?php
+        if ($relatorios) :
+          foreach ($relatorios as $relatorio) :
+            echo "['" . $relatorio['modificacao'] . "', " . $relatorio['q13'] . "],";
+          endforeach;
+        endif;
+      ?>
+    ]);
+
+    var config = {
+        title:'Atividade (Questão 13/15)',
+        width:700,
+        height:300,
+        legend: { position: "none" },
+        colors: ['#1a237e'],
+        vAxis: {
+          minValue: 0, 
+          ticks: [0, 1, 2, 3, 4]
+        },
+    };
+
+    var chart = new google.visualization.ColumnChart(document.getElementById('grafico_q13'));
+
+    chart.draw(dados, config);
+  }
+</script>
+<script type="text/javascript">
+
+  google.load('visualization', '1.0', {'packages':['corechart']});
+
+  google.setOnLoadCallback(desenharGrafico);
+
+  function desenharGrafico() {
+    var dados = new google.visualization.DataTable();
+    dados.addColumn('string', 'Data');
+    dados.addColumn('number', 'Resposta');
+    dados.addRows([
+      <?php
+        if ($relatorios) :
+          foreach ($relatorios as $relatorio) :
+            echo "['" . $relatorio['modificacao'] . "', " . $relatorio['q14'] . "],";
+          endforeach;
+        endif;
+      ?>
+    ]);
+
+    var config = {
+        title:'Grau e consistência das respostas da inteligência (Questão 14/15)',
+        width:700,
+        height:300,
+        legend: { position: "none" },
+        colors: ['#1a237e'],
+        vAxis: {
+          minValue: 0, 
+          ticks: [0, 1, 2, 3, 4]
+        },
+    };
+
+    var chart = new google.visualization.ColumnChart(document.getElementById('grafico_q14'));
+
+    chart.draw(dados, config);
+  }
+</script>
+<script type="text/javascript">
+
+  google.load('visualization', '1.0', {'packages':['corechart']});
+
+  google.setOnLoadCallback(desenharGrafico);
+
+  function desenharGrafico() {
+    var dados = new google.visualization.DataTable();
+    dados.addColumn('string', 'Data');
+    dados.addColumn('number', 'Resposta');
+    dados.addRows([
+      <?php
+        if ($relatorios) :
+          foreach ($relatorios as $relatorio) :
+            echo "['" . $relatorio['modificacao'] . "', " . $relatorio['q15'] . "],";
+          endforeach;
+        endif;
+      ?>
+    ]);
+
+    var config = {
+        title:'Impressão geral (Questão 15/15)',
+        width:700,
+        height:300,
+        legend: { position: "none" },
+        colors: ['#1a237e'],
+        vAxis: {
+          minValue: 0, 
+          ticks: [0, 1, 2, 3, 4]
+        },
+    };
+
+    var chart = new google.visualization.ColumnChart(document.getElementById('grafico_q15'));
 
     chart.draw(dados, config);
   }
 </script>
 
 <div class="row center">
-  <div class="col l6 m6 s12" id="area_grafico"></div>
-  <div class="col l6 m6 s12" id="area_grafico2"></div>
+  <div class="col l6 m6 s12" id="grafico_total"></div>
+  <div class="col l6 m6 s12" id="grafico_q1"></div>
+</div>
+<div class="row center">
+  <div class="col l6 m6 s12" id="grafico_q2"></div>
+  <div class="col l6 m6 s12" id="grafico_q3"></div>
+</div>
+<div class="row center">
+  <div class="col l6 m6 s12" id="grafico_q4"></div>
+  <div class="col l6 m6 s12" id="grafico_q5"></div>
+</div>
+<div class="row center">
+  <div class="col l6 m6 s12" id="grafico_q6"></div>
+  <div class="col l6 m6 s12" id="grafico_q7"></div>
+</div>
+<div class="row center">
+  <div class="col l6 m6 s12" id="grafico_q8"></div>
+  <div class="col l6 m6 s12" id="grafico_q9"></div>
+</div>
+<div class="row center">
+  <div class="col l6 m6 s12" id="grafico_q10"></div>
+  <div class="col l6 m6 s12" id="grafico_q11"></div>
+</div>
+<div class="row center">
+  <div class="col l6 m6 s12" id="grafico_q12"></div>
+  <div class="col l6 m6 s12" id="grafico_q13"></div>
+</div>
+<div class="row center">
+  <div class="col l6 m6 s12" id="grafico_q14"></div>
+  <div class="col l6 m6 s12" id="grafico_q15"></div>
 </div>
 
 <?php include(FOOTER_TEMPLATE); ?>
