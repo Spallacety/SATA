9a8fc687c9622176e6949eb31061c71c66a231d9
Spallacety <spallacety@hotmail.com>
2017-06-13 11:14:34 -0300

    commit

--- cadastros/pacientes/view.php
+++ cadastros/pacientes/view.php
@@ -11,9 +11,9 @@
 <div class="row">
   <div class="col s12">
     <ul class="tabs">
-      <li class="tab col s3"><a href="#test1">Test 1</a></li>
-      <li class="tab col s3"><a class="active" href="#test2">Test 2</a></li>
-      <li class="tab col s3 disabled"><a href="#test3">Disabled Tab</a></li>
+      <li class="tab col s3"><a class="active" href="#test1">Dados Gerais</a></li>
+      <li class="tab col s3"><a href="#test2">Dados Clinicos</a></li>
+      <li class="tab col s3"><a href="#test3">Avaliacoes</a></li>
       <li class="tab col s3"><a href="#test4">Test 4</a></li>
     </ul>
   </div>
