05b47a37ecc23ffb132fe633a3e11f2f4bd94bec
Lucas R <spallacety@hotmail.com>
2017-06-18 01:54:32 -0300

    a

--- avaliacao/cars/view.php
+++ avaliacao/cars/view.php
@@ -24,7 +24,7 @@
         findAllCars($_GET['id']);
         if ($allcars) :
           foreach ($allcars as $avaliacao) :
-            $geral['q1'] = ( (float) $avaliacao['q1'] + $geral['q1'] ) / 2;
+            $geral['q1'] += (float) $avaliacao['q1'];
             $geral['q2'] += (float) $avaliacao['q2'];
             $geral['q3'] += (float) $avaliacao['q3'];
             $geral['q4'] += (float) $avaliacao['q4'];
@@ -41,8 +41,8 @@
             $geral['q15'] += (float) $avaliacao['q15'];
           endforeach;
         endif;
-        echo "['1', " . $geral['q1'] . ", 'ao vivo'],";
-        echo "['2', " . $geral['q2'] . ", 'ao vivo'],";
+        echo "['1', " . echo round($geral['q1']*2)/2 . ", 'ao vivo'],";
+        echo "['2', " . echo round($geral['q2']*2)/2 . ", 'ao vivo'],";
         echo "['3', " . $geral['q3'] . ", 'ao vivo'],";
         echo "['4', " . $geral['q4'] . ", 'ao vivo'],";
         echo "['5', " . $geral['q5'] . ", 'ao vivo'],";
