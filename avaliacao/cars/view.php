9ac9ef5808939ec410f59258f0fcd29cac4e0eae
Lucas R <spallacety@hotmail.com>
2017-06-18 02:57:58 -0300

    a

--- avaliacao/cars/view.php
+++ avaliacao/cars/view.php
@@ -78,7 +78,7 @@
 <h4 class="main-text center">Relatórios de <?php echo $paciente['nome'] ?></h2>
 <hr>
 
-<div class="chart" id="grafico_geral" style="align: center;"></div>
+<div class="chart" id="grafico_geral"></div>
 
 <div class="container">
   <div class="row center">
