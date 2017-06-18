2f53caea2d5ede4c4489a78a4c84b5d4de8bd0e7
Lucas R <spallacety@hotmail.com>
2017-06-18 00:55:21 -0300

    a

--- phpMyAdmin/auth.php
+++ phpMyAdmin/auth.php
@@ -1,5 +1,42 @@
 <?php 
 
+function find( $table = null, $id = null ) {
+  
+  $database = open_database();
+  $found = null;
+  try {
+    if ($id) {
+      $sql = "SELECT * FROM " . $table . " WHERE id = " . $id;
+      $result = $database->query($sql);
+      
+      if ($result->num_rows > 0) {
+        $found = $result->fetch_assoc();
+      }
+      
+    } else {
+      
+      $sql = "SELECT * FROM " . $table;
+      $result = $database->query($sql);
+      
+      if ($result->num_rows > 0) {
+        $found = $result->fetch_all(MYSQLI_ASSOC);
+        
+        /* Metodo alternativo
+        $found = array();
+        while ($row = $result->fetch_assoc()) {
+          array_push($found, $row);
+        } */
+      }
+    }
+  } catch (Exception $e) {
+    $_SESSION['message'] = $e->GetMessage();
+    $_SESSION['type'] = 'danger';
+  }
+  
+  close_database($database);
+  return $found;
+}
+
 function verifyIfAdmin( $id ){
   $user = find('profissionais', $id);
   if ($user['nivelacesso'] == 1){
