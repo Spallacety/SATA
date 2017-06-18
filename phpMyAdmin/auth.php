b5ea92fa06f10d2de76d38a64174bcc0f5fdfc00
Lucas R <spallacety@hotmail.com>
2017-06-18 00:54:47 -0300

    a

--- phpMyAdmin/auth.php
+++ phpMyAdmin/auth.php
@@ -1,6 +1,12 @@
 <?php 
 
-require_once DBAPI;
+function verifyIfAdmin( $id ){
+  $user = find('profissionais', $id);
+  if ($user['nivelacesso'] == 1){
+    return 1;
+  }
+  return 0;
+}
 
 $verify = verifyIfAdmin($_COOKIE['id_profissional']);
 
