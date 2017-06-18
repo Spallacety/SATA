5c8ed716d189cbe695e682104f195f9b3801bf93
Lucas R <spallacety@hotmail.com>
2017-06-17 22:03:17 -0300

    a

--- avaliacao/cars/index.php
+++ avaliacao/cars/index.php
@@ -21,15 +21,21 @@
 	</thead>
 	<tbody>
 		<?php if ($resultados) : ?>
-		<?php foreach ($resultados as $resultado) : ?>
+		<?php foreach ($pacientes as $paciente) : ?>
 		<tr>
 			<td> 
-        <?php echo $resultado['nome']; ?>
+        <?php echo $paciente['nome']; ?>
       </td>
    		<td>
    			<?php $profissional = find('profissionais', $_COOKIE['id_profissional']);?>
    			<?php echo $profissional['nome']; ?>
    		</td>
+      <td class="row vallign right-align">
+        <div class="btn-group-h">
+          <a href="/avaliacao/cars/add.php?id=<?php echo $paciente['id']; ?>" class="btn-flat img-btn tooltipped" data-position="bottom" data-delay="50" data-tooltip="Nova Avaliação"><i class="material-icons blue-text">add</i></a>
+          <a href="view.php?id=<?php echo $paciente['id']; ?>" class="btn-flat img-btn tooltipped" data-position="bottom" data-delay="50" data-tooltip="Ver Relatórios"><i class="material-icons green-text">pie_chart</i></a>
+        </div>
+      </td>
     </tr>
 		<?php endforeach; ?>
 		<?php else : ?>
