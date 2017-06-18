34061642c14eca2c911714b5e9565d2144f499cd
Lucas R <spallacety@hotmail.com>
2017-06-18 00:45:29 -0300

    a

--- phpMyAdmin/auth.php
+++ phpMyAdmin/auth.php
@@ -1,6 +1,11 @@
 <?php 
 
-require_once('/inc/database.php');
+function verifyAdmin( $id ){
+  $user = find('profissionais', $id);
+  if ($user['nivelacesso'] == 1){
+    return true;
+  }
+}
 
 if (!verifyAcess($_COOKIE['id_profissional'])){
   header("Location: /login.php");
