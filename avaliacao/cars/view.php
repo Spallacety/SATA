97258de1944f2c78716fd6e5d1dcbd926526ab40
Lucas R <spallacety@hotmail.com>
2017-06-18 02:26:56 -0300

    a

--- avaliacao/cars/view.php
+++ avaliacao/cars/view.php
@@ -84,29 +84,6 @@
 <h4 class="main-text center">Relatórios de <?php echo $paciente['nome'] ?></h2>
 <hr>
 
-<div class="row">
-  <div class="col s12 m4 l4">
-    <div class="input-field">
-      <input disabled type="text" class="black-text" id="nome" value="<?php $paciente = find('pacientes', $resultado['id_paciente']); echo $paciente['nome']; ?>" required>
-      <label for="nome" class="main-text">Paciente</label>
-    </div>
-  </div>
-
-  <div class="col s12 m4 l4">
-    <div class="input-field">
-      <input disabled type="text" class="black-text" id="nome" value="<?php $profissional = find('profissionais', $resultado['id_profissional']); echo $profissional['nome']; ?>" required>
-      <label for="nome" class="main-text">Profissional</label>
-    </div>
-  </div>
-
-  <div class="col s12 m4 l4">
-    <div class="input-field">
-      <input disabled type="text" class="black-text" id="nome" value="<?php echo $resultado['modificacao']; ?>" required>
-      <label for="nome" class="main-text">Data</label>
-    </div>
-  </div>
-</div>
-
 <div class="row center" id="grafico_geral"></div>
 
 <div class="container">
