6652f9127700472308af4f5ab605eebb7a38fb20
Spallacety <spallacety@hotmail.com>
2017-06-22 13:57:51 -0300

    a

--- index.php
+++ index.php
@@ -33,7 +33,7 @@ if ($_COOKIE){
     </div>
     <br>
     <div class="row center">
-      <input id="login-button" class="btn waves-effect waves-light main-color" name="login-button" type="submit" value="Acessar"></input>
+      <button type="submit" class="btn waves-effect waves-light main-color">Acessar</button>
     </div>
     <br>
   </form>
