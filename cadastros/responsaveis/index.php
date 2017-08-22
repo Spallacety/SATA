9427bad82f4bd73054e9b2a084d55a34f5108823
Spallacety <spallacety@hotmail.com>
2017-08-22 13:55:28 -0300

    A

--- cadastros/responsaveis/index.php
+++ cadastros/responsaveis/index.php
@@ -8,12 +8,12 @@
 
 <div class="row valign-wrapper">
 	<div class="col s12 m6 valign left-align">
-		<h4 class="main-text">Profissionais</h4>
+		<h4 class="main-text">Responsáveis</h4>
 	</div>
 	<div class="col s12 m6 valign right-align">
     <div class="row right">
     	<br>
-		<a href="add.php" class="btn-floating white-text blue waves-effect waves-light tooltipped" data-position="bottom" data-delay="50" data-tooltip="Novo profissional"><i class="material-icons">add</i></a>
+		<a href="add.php" class="btn-floating white-text blue waves-effect waves-light tooltipped" data-position="bottom" data-delay="50" data-tooltip="Novo responsável"><i class="material-icons">add</i></a>
 		<a href="index.php" class="btn-floating white-text blue waves-effect waves-light tooltipped" data-position="bottom" data-delay="50" data-tooltip="Atualizar"><i class="material-icons">refresh</i></a>
   	</div>
 	</div>
@@ -24,25 +24,25 @@
 	<thead>
 		<tr>
 			<th width="45%">Nome</th>
-			<th width="15%">Profissão</th>
+			<th width="15%">Paciente</th>
 			<th width="40%" class="right-align"></th>
 		</tr>
 	</thead>
 	<tbody>
-		<?php if ($profissionais) : ?>
-		<?php foreach ($profissionais as $profissional) : ?>
-    <?php if ($profissional['id'] > 1) : ?>
+		<?php if ($responsaveis) : ?>
+		<?php foreach ($responsaveis as $responsavel) : ?>
+    <?php if ($responsavel['id'] > 1) : ?>
 		<tr>
-			<td><?php echo $profissional['nome']; ?></td>		
+			<td><?php echo $responsavel['nome']; ?></td>		
    		<td>
-   			<?php $profissao = find('profissoes', $profissional['id_profissao']);?>
+   			<?php $profissao = find('profissoes', $responsavel['id_profissao']);?>
    			<?php echo $profissao['descricao']?>
    		</td>
 			<td class="row vallign right-align">
 				<div class="btn-group-h">
-					<a href="view.php?id=<?php echo $profissional['id']; ?>" class="btn-flat img-btn tooltipped" data-position="bottom" data-delay="50" data-tooltip="Visualizar"><i class="material-icons green-text">visibility</i></a>
-					<a href="edit.php?id=<?php echo $profissional['id']; ?>" class="btn-flat img-btn tooltipped" data-position="bottom" data-delay="50" data-tooltip="Editar"><i class="material-icons orange-text">mode_edit</i></a>
-					<a href="#!" onClick="abrirModalApagarProfissional(<?php echo $profissional['id']; ?>, '<?php echo $profissional['nome']; ?>');" class="btn-flat img-btn tooltipped" data-position="bottom" data-delay="50" data-tooltip="Apagar"><i class="material-icons red-text">delete</i></a>
+					<a href="view.php?id=<?php echo $responsavel['id']; ?>" class="btn-flat img-btn tooltipped" data-position="bottom" data-delay="50" data-tooltip="Visualizar"><i class="material-icons green-text">visibility</i></a>
+					<a href="edit.php?id=<?php echo $responsavel['id']; ?>" class="btn-flat img-btn tooltipped" data-position="bottom" data-delay="50" data-tooltip="Editar"><i class="material-icons orange-text">mode_edit</i></a>
+					<a href="#!" onClick="abrirModalApagarresponsavel(<?php echo $responsavel['id']; ?>, '<?php echo $responsavel['nome']; ?>');" class="btn-flat img-btn tooltipped" data-position="bottom" data-delay="50" data-tooltip="Apagar"><i class="material-icons red-text">delete</i></a>
 				</div>
 			</td>
 		</tr>
